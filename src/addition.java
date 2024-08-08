b51f3ca33f516b46840d91cb8f5f1cedc5a7158b
Miraj8445 <alimiraj078600@gmail.com>
2024-08-09 01:24:14 +0530

    Basicprogram

---
+++ src/addition.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class addition {
+    public static void main(String[] args){
+        Scanner scr = new Scanner(System.in);
+
+        int a = scr.nextInt();
+        int b = scr.nextInt();
+
+        int Sum = a+b;
+
+        System.out.println("Your sum value is = " + Sum);
+    }
+}
